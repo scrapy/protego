e261cb59b12397af3f4ee1cd581843304800f196
Anubhav Patel <anubhavp28@gmail.com>
2019-06-23 15:39:52 +0530

    adds test infrastructure

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='Protego',
+      version='0.1dev',
+      description='Pure python robots.txt parser with support for modern conventions',
+      author='Anubhav Patel',
+      author_email='anubhavp28@gmail.com',
+      package_dir={'': 'src'},
+      packages=find_packages('src'),
+      py_modules=['protego'],
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+      install_requires=[],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      )
--- tests/test_recordgroup.py
+++ tests/test_recordgroup.py
@@ -1,5 +1,6 @@
 from protego import RecordGroup
 
 
-class RecordGroupTest:
-    pass
+class TestRecordGroup():
+    def test_sample(self):
+        assert 3 == 3
