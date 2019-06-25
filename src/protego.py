ca529457aa9fbf43958fb55943054685872281f4
Anubhav Patel <anubhavp28@gmail.com>
2019-06-25 14:04:54 +0530

    impl for standard directives + adds tests

--- src/protego.py
+++ src/protego.py
@@ -1,109 +1,140 @@
+from collections import namedtuple
+from six.moves.urllib.parse import urlparse
+
+Record = namedtuple('Record', ['field', 'value'])
+
+
 class RecordGroup:
 
     def __init__(self):
         self.user_agents = []
-        self.rules = {}
-        self.sorted_allow_disallow = None
+        self.rules = []
+        self.crawl_delay = None
+        self.sorted_rules = None
 
     def add_user_agent(self, user_agent):
-        self.user_agents.append(user_agent.lower())
+        self.user_agents.append(user_agent.strip().lower())
 
-    def applies_to(self, robot):
+    def applies_to(self, robotname):
+        robotname = robotname.strip().lower()
         for user_agent in self.user_agents:
-            if robot in user_agent:
+            if (robotname in user_agent) or (user_agent == '*'):
                 return True
         return False
 
     def allow(self, path):
-        if 'allow' not in self.rules:
-            self.rules['allow'] = []
-        self.rules['allow'].append(path)
+        self.rules.append(Record(field='allow', value=path))
 
     def disallow(self, path):
-        if 'disallow' not in self.rules:
-            self.rules['disallow'] = []
-        self.rules['disallow'].append(path)
+        self.rules.append(Record(field='disallow', value=path))
 
-    def can_fetch(self, path):
-        if not self.sorted_allow_disallow:
-            self.sorted_allow_disallow = []
-            for p in self.rules['allow']:
-                self.sorted_allow_disallow.append((p, 'allow'))
-            for p in self.rules['disallow']:
-                self.sorted_allow_disallow.append((p, 'disallow'))
+    def allowed(self, url):
+        if not self.sorted_rules:
+            self.sorted_rules = []
+            self.sorted_rules = sorted(
+                self.rules, key=lambda r: len(r.value), reverse=True)
 
-            def pathlength(t):
-                return len(t[0])
-            sorted(self.sorted_allow_disallow, key=pathlength, reverse=True)
+        path = urlparse(url).path
 
-        for p, d in self.sorted_allow_disallow:
-            if path.startswith(p):
-                return True if d == 'allow' else False
-        return True
+        allowed = True
+        for record in self.sorted_rules:
+            if path.startswith(record.value):
+                if record.field == 'disallow':
+                    allowed = False
+                break
+
+        return allowed
 
     def set_crawl_delay(self, delay):
-        self.rules['crawl-delay'] = delay
+        self.crawl_delay = delay
 
     def get_crawl_delay(self):
-        if 'crawl-delay' not in self.rules:
-            return None
-        return self.rules['crawl-delay']
-
-    def add_sitemap(self, path):
-        if 'sitemaps' not in self.rules:
-            self.rules['sitemaps'] = []
-        self.rules['sitemaps'].append(path)
-
-    def get_sitemaps(self):
-        if 'sitemaps' not in self.rules:
-            return iter(())
-        return iter(self.rules['sitemaps'])
-
-    def set_preferred_host(self, host):
-        self.rules['host'] = host
-
-    def get_preferred_host(self):
-        if 'host' not in self.rules:
-            return None
-        return self.rules['host']
+        return self.crawl_delay
 
 
 class Protego:
 
     def __init__(self):
         self.record_groups = []
+        self.host = None
+        self.sitemap_list = []
 
-    def _parse(self, content):
+    @classmethod
+    def parse(cls, content):
+        o = cls()
+        o._parse_robotstxt(content)
+        return o
+
+    def _parse_robotstxt(self, content):
         lines = content.split('\n')
         current_record_group = None
-        previous_directive = None
+        previous_record = None
 
         for line in lines:
             hash_pos = line.find('#')
             if hash_pos != -1:
-                line = line[0: hash_pos]
-                if not line:
-                    continue
+                line = line[0: hash_pos].strip()
 
             line = line.strip()
-            directive, value = line.split(':')
-            directive = directive.strip().lower()
-            value = value.strip()
-
-            if not current_record_group and directive != 'user-agent':
+            if not line:
                 continue
 
-            if directive == 'user-agent':
+            line = line.strip()
+            field, value = line.split(':', 1)
+            field = field.strip().lower()
+            value = value.strip()
+
+            if not current_record_group and field != 'user-agent':
+                continue
+
+            if field == 'user-agent':
+                if previous_record and previous_record.field != 'user-agent':
+                    current_record_group = None
+
                 if not current_record_group:
                     current_record_group = RecordGroup()
+                    self.record_groups.append(current_record_group)
+
                 current_record_group.add_user_agent(value)
-            elif directive == 'allow':
+
+            elif field == 'allow':
                 current_record_group.allow(value)
-            elif directive == 'disallow':
+
+            elif field == 'disallow':
                 current_record_group.disallow(value)
-            elif directive == 'sitemap':
-                current_record_group.add_sitemap(value)
-            elif directive == 'crawl-delay':
-                current_record_group.set_crawl_delay(value)
-            elif directive == 'host':
-                current_record_group.set_preferred_host(value)
+
+            elif field == 'sitemap':
+                self.sitemap_list.append(value)
+
+            elif field == 'crawl-delay':
+                current_record_group.set_crawl_delay(float(value))
+
+            elif field == 'host':
+                self.host = value
+
+            previous_record = Record(field, value)
+
+        for record_group in self.record_groups:
+            if '*' in record_group.user_agents:
+                self.record_groups.sort(key=lambda o: o is record_group)
+                break
+
+    def allowed(self, url, user_agent):
+        for record_group in self.record_groups:
+            if record_group.applies_to(user_agent):
+                return record_group.allowed(url)
+
+        return True
+
+    def crawl_delay(self, user_agent):
+        for record_group in self.record_groups:
+            if record_group.applies_to(user_agent):
+                return record_group.get_crawl_delay()
+
+        return None
+
+    def sitemaps(self):
+        return iter(self.sitemap_list)
+
+    def preferred_host(self):
+        return self.host
--- tests/test_recordgroup.py
+++ tests/test_recordgroup.py
@@ -1,6 +1,99 @@
-from protego import RecordGroup
+# encoding=utf-8
+from protego import Protego
+from unittest import TestCase
 
 
-class TestRecordGroup():
-    def test_sample(self):
-        assert 3 == 3
+class TestProtego(TestCase):
+    def test_allowed(self):
+        robotstxt_content = ("User-agent: * \n"
+                             "Disallow: /disallowed \n"
+                             "Allow: /allowed \n"
+                             "Crawl-delay: 10")
+        rp = Protego.parse(content=robotstxt_content)
+        self.assertTrue(rp.allowed("https://www.site.local/allowed", "*"))
+        self.assertFalse(rp.allowed("https://www.site.local/disallowed", "*"))
+
+    def test_length_based_precedence(self):
+        robotstxt_content = ("User-agent: * \n"
+                             "Disallow: / \n"
+                             "Allow: /page")
+        rp = Protego.parse(content=robotstxt_content)
+        self.assertTrue(rp.allowed("https://www.site.local/page", "*"))
+
+    def test_sitemaps(self):
+        robotstxt_content = ("User-agent: * \n"
+                             "Disallow: /disallowed \n"
+                             "Allow: /allowed \n"
+                             "Sitemap: https://site.local/sitemap.xml")
+        rp = Protego.parse(
+            content=robotstxt_content)
+        sitemaps = list(rp.sitemaps())
+        self.assertTrue(len(sitemaps) == 1)
+        self.assertTrue("https://site.local/sitemap.xml" in sitemaps)
+
+    def test_no_sitemaps(self):
+        robotstxt_content = ("User-agent: * \n"
+                             "Disallow: /disallowed \n"
+                             "Allow: /allowed \n"
+                             "Crawl-delay: 10")
+        rp = Protego.parse(
+            content=robotstxt_content)
+        self.assertTrue(not list(rp.sitemaps()))
+
+    def test_no_preferred_host(self):
+        robotstxt_content = ("User-agent: * \n"
+                             "Disallow: /disallowed \n"
+                             "Allow: /allowed \n"
+                             "Crawl-delay: 10")
+        rp = Protego.parse(
+            content=robotstxt_content)
+        self.assertTrue(rp.preferred_host() is None)
+
+    def test_crawl_delay(self):
+        robotstxt_content = ("User-agent: * \n"
+                             "Disallow: /disallowed \n"
+                             "Allow: /allowed \n"
+                             "Crawl-delay: 10")
+        rp = Protego.parse(
+            content=robotstxt_content)
+        self.assertTrue(rp.crawl_delay('*') == 10.0)
+
+    def test_no_crawl_delay(self):
+        robotstxt_content = ("User-agent: * \n"
+                             "Disallow: /disallowed \n"
+                             "Allow: /allowed")
+        rp = Protego.parse(
+            content=robotstxt_content)
+        self.assertTrue(rp.crawl_delay('*') is None)
+
+    def test_empty_response(self):
+        """empty response should equal 'allow all'"""
+        rp = Protego.parse(content='')
+        self.assertTrue(rp.allowed("https://site.local/", "*"))
+        self.assertTrue(rp.allowed("https://site.local/", "chrome"))
+        self.assertTrue(rp.allowed("https://site.local/index.html", "*"))
+        self.assertTrue(rp.allowed("https://site.local/disallowed", "*"))
+
+    def test_unicode_url_and_useragent(self):
+        robotstxt_content = u"""
+        User-Agent: *
+        Disallow: /admin/
+        Disallow: /static/
+        # taken from https://en.wikipedia.org/robots.txt
+        Disallow: /wiki/K%C3%A4ytt%C3%A4j%C3%A4:
+        Disallow: /wiki/Käyttäjä:
+        User-Agent: UnicödeBöt
+        Disallow: /some/randome/page.html"""
+        rp = Protego.parse(
+            content=robotstxt_content)
+        self.assertTrue(rp.allowed("https://site.local/", "*"))
+        self.assertFalse(rp.allowed("https://site.local/admin/", "*"))
+        self.assertFalse(rp.allowed("https://site.local/static/", "*"))
+        self.assertTrue(rp.allowed("https://site.local/admin/", u"UnicödeBöt"))
+        self.assertFalse(rp.allowed(
+            "https://site.local/wiki/K%C3%A4ytt%C3%A4j%C3%A4:", "*"))
+        self.assertFalse(rp.allowed(u"https://site.local/wiki/Käyttäjä:", "*"))
+        self.assertTrue(rp.allowed(
+            "https://site.local/some/randome/page.html", "*"))
+        self.assertFalse(rp.allowed(
+            "https://site.local/some/randome/page.html", u"UnicödeBöt"))
