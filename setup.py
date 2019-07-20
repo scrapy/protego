faf12ccb73aa8a135ef7ab3b1fa1307afbfc0cd6
Anubhav Patel <anubhavp28@gmail.com>
2019-07-20 15:24:15 +0530

    hope this works

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(name='Protego',
       python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
       install_requires=['six'],
       tests_require=['pytest'],
+      include_package_data=True,
       keywords=['robots.txt', 'parser', 'robots', 'rep'],
       classifiers=[
           'Framework :: Scrapy',
